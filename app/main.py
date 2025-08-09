399258843c1a047fb73441925c36b1777bafaf66
Hesham Abdelfattah <hesham.abdelfattah81@gmail.com>
2025-08-09 17:04:59 +0200

    updategpt versio

--- app/main.py
+++ app/main.py
@@ -27,7 +27,7 @@ def analyze_match_with_ai(job_requirements, candidate_profile):
         """
         
         response = client.chat.completions.create(
-            model="gpt-3.5-turbo",
+            model="gpt-4o",
             messages=[{"role": "user", "content": prompt}],
             max_tokens=200,
             temperature=0.7
